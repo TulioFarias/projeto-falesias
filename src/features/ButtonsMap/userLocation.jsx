fcac69ace7494f0547f7568b4fd582db6bf683fa
TulioFarias <tuliott10@gmail.com>
2024-06-25 14:14:47 -0300

    update

--- src/features/ButtonsMap/userLocation.jsx
+++ src/features/ButtonsMap/userLocation.jsx
@@ -1,75 +1,110 @@
-import LocationOnIcon from '@mui/icons-material/LocationOn'
-import { Tooltip } from '@mui/material'
-import Feature from 'ol/Feature'
-import Point from 'ol/geom/Point'
-import { Vector as VectorLayer } from 'ol/layer'
-import { Circle as CircleStyle, Fill, Stroke, Style, Icon } from 'ol/style'
-import React from 'react'
+import LocationOnIcon from '@mui/icons-material/LocationOn';
+import { Tooltip } from '@mui/material';
+import Feature from 'ol/Feature';
+import Point from 'ol/geom/Point';
+import { Vector as VectorLayer } from 'ol/layer';
+import { Icon } from 'ol/style';
+import { Vector as VectorSource } from 'ol/source'; 
+import React, { useState } from 'react';
+import { Circle as CircleStyle, Fill, Stroke, Style } from 'ol/style'
+import { mapInstance } from '../../config/layers/showmap';
+import { vector } from '../../config/layers/vector';
+import locationIcon from '../../assets/img/personicon.svg';
+import apiGoogle from '../../services/apiGeolocationGoogle';
 
-import { mapInstance } from '../../config/layers/showmap'
-import { vector } from '../../config/layers/vector'
-import locationIcon from '../../assets/img/personicon.svg'
-
+const apiKey = apiGoogle
 
 function FindUser() {
+  const [watchId, setWatchId] = useState(null);
+
   const buscarLocalizacao = async () => {
-    useEffect(() => {
-      let watchId = null;
-  
-      const buscarLocalizacao = async () => {
-        try {
-          if (navigator.geolocation) {
-            watchId = navigator.geolocation.watchPosition(position => {
-              const { latitude, longitude } = position.coords
-  
-              const coords = [longitude, latitude]
-              const point = new Point(coords)
-              const pointFeature = new Feature(point)
-              const vectorStyle = new Icon({
-                anchor: [0.5, 1],
-                src: locationIcon,
-                scale: 0.1 
-              })
-  
-              const markerLayer = new VectorLayer({
-                source: vector.getSource(),
-                style: vectorStyle,
-              })
-  
-              vector.getSource().clear()
-              vector.getSource().addFeature(pointFeature)
-  
-              mapInstance.addLayer(markerLayer)
-              mapInstance.getView().setCenter(coords)
-              mapInstance.getView().setZoom(20)
-            })
-          } else {
-            alert('O navegador não suporta geolocalização.')
-          }
-        } catch (error) {
-          console.error('Erro ao obter localização:', error)
-        }
+    try {
+      const response = await fetch(`https://www.googleapis.com/geolocation/v1/geolocate?key=${apiKey}`, {
+        method: 'POST',
+        headers: {
+          'Content-Type': 'application/json'
+        },
+        body: JSON.stringify({
+          considerIp: true
+        })
+      });
+
+
+      if (!response.ok) {
+        throw new Error('Erro ao obter a localização');
       }
 
-      buscarLocalizacao();
+      const data = await response.json();
+      const { location } = data;
 
-      return () => {
-        if (watchId !== null) {
-          navigator.geolocation.clearWatch(watchId);
-        }
-      };
-    }, []);
-  }
+      const coords = [location.lng, location.lat];
+      const point = new Point(coords);
+      const pointFeature = new Feature(point);
+      
+
+      const vectorStyle = [
+        new Style({
+          fill: new Fill({
+            color: 'rgba(255, 255, 255, 0.2)'
+          }),
+          image: new CircleStyle({
+            radius: 5,
+            fill: new Fill({
+              color: 'rgba(169,0,201,1)'
+            })
+          })
+        }),
+
+        new Style({
+          image: new CircleStyle({
+            radius: 70,
+            fill: new Fill({
+              color: 'rgba(255,255,255,0.4)'
+            }),
+            stroke: new Stroke({
+              color: 'rgba(169,0,201,1)',
+              width: 2
+            })
+          })
+        })
+      ]
+
+
+   
+      const markerLayer = new VectorLayer({
+        style: vectorStyle 
+      });
+
+      vector.getSource().clear();
+      vector.getSource().addFeature(pointFeature);
+
+      mapInstance.addLayer(markerLayer);
+      mapInstance.getView().setCenter(coords);
+      mapInstance.getView().setZoom(20);
+    } catch (error) {
+      console.error('Erro ao obter localização:', error);
+    }
+  };
+
+  const handleButtonClick = () => {
+    if (watchId === null) {
+      buscarLocalizacao();
+      setWatchId(setInterval(buscarLocalizacao, 5000));
+    } else {
+      clearInterval(watchId);
+      setWatchId(null);
+    }
+  };
 
   return (
     <div>
       <Tooltip title="Mostrar sua localização" placement="left">
-        <button onClick={buscarLocalizacao} className="my-custom-btns">
+        <button onClick={handleButtonClick} className="my-custom-btns">
           <LocationOnIcon />
         </button>
       </Tooltip>
     </div>
-  )
+  );
 }
 
-export default FindUser
+export default FindUser;
---
+++ src/services/apiGeolocationGoogle.jsx
@@ -0,0 +1,5 @@
+
+
+export const apiGoogle = 'AIzaSyATGZDzDfK1X3IFyJ7qBHa-eR8XdrcQKR8'
+
+export default apiGoogle 
